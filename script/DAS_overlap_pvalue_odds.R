5adfe87cbd0d01444890d967ff076a85f864c57a
Fu Xing <bio.xfu@gmail.com>
2018-10-26 17:18:20 +0800

    add GO anlaysis of DAS overlap genes

--- script/DAS_overlap_pvalue_odds.R
+++ script/DAS_overlap_pvalue_odds.R
@@ -2,11 +2,32 @@ library(magrittr)
 library(GeneOverlap)
 library(gplots)
 library(plotrix)
+library(topGO)
+library(GO.db)
+
+topGO <- function(myGenes, category='BP', p_cutoff=0.05, gomap, geneid){
+  geneID2GO <- readMappings(file = gomap)
+  geneNames <- read.table(geneid, stringsAsFactors=F)$V1
+  geneList <- factor(as.integer(geneNames %in% myGenes))
+  names(geneList) <- geneNames
+  GOdata <- new("topGOdata", ontology=category, allGenes=geneList, annot=annFUN.gene2GO, gene2GO=geneID2GO)
+  resultFisher <- runTest(GOdata, algorithm = "weight", statistic = "fisher")
+  allRes <- GenTable(GOdata,pvalue=resultFisher,topNodes=100)
+  allRes$pvalue[grep('<',allRes$pvalue)] <- "1e-30"
+  allRes$pvalue <- as.numeric(allRes$pvalue)
+  allRes <- allRes[order(allRes$pvalue,decreasing=F),]
+  allRes$catagory <- category
+  allRes <- allRes[allRes$pvalue < p_cutoff, ]
+  return(allRes)
+}
 
 argv <- commandArgs(T)
 psi_cutoff <- argv[1]
 KO_path <- argv[2]
 MT_path <- argv[3]
+# psi_cutoff <- 0.1
+# KO_path <- '/data1/HJY/Project/Mecp2/KO/AltSplicing2_124/pairadise/WT_vs_KO/'
+# MT_path <- '/data1/HJY/Project/Mecp2/T158M/AltSplicing2_134/pairadise/WT_vs_T158/'
 KO_all <- NULL
 MT_all <- NULL
 
@@ -14,24 +35,24 @@ for (AS in c('SE', 'RI', 'MXE', 'A3SS', 'A5SS')) {
   KO <- read.table(paste0(KO_path, '/', AS, '.MATS.JCEC.paired.compared.txt'), header = T, sep = '\t')
   MT <- read.table(paste0(MT_path, '/', AS, '.MATS.JCEC.paired.compared.txt'), header = T, sep = '\t')
   if (AS == 'SE') {
-    KO$event <- paste(KO$chr, KO$strand, KO$exonStart_0base, KO$exonEnd, KO$upstreamES, KO$upstreamEE, KO$downstreamES, KO$downstreamEE, sep='|')
-    MT$event <- paste(MT$chr, MT$strand, MT$exonStart_0base, MT$exonEnd, MT$upstreamES, MT$upstreamEE, MT$downstreamES, MT$downstreamEE, sep='|')
+    KO$event <- paste('SE', KO$chr, KO$strand, KO$exonStart_0base, KO$exonEnd, KO$upstreamES, KO$upstreamEE, KO$downstreamES, KO$downstreamEE, KO$geneSymbol, KO$GeneID, sep='|')
+    MT$event <- paste('SE', MT$chr, MT$strand, MT$exonStart_0base, MT$exonEnd, MT$upstreamES, MT$upstreamEE, MT$downstreamES, MT$downstreamEE, MT$geneSymbol, MT$GeneID, sep='|')
   }
   if (AS == 'RI') {
-    KO$event <- paste(KO$chr, KO$strand, KO$riExonStart_0base, KO$riExonEnd, KO$upstreamES, KO$upstreamEE, KO$downstreamES, KO$downstreamEE, sep='|')
-    MT$event <- paste(MT$chr, MT$strand, MT$riExonStart_0base, MT$riExonEnd, MT$upstreamES, MT$upstreamEE, MT$downstreamES, MT$downstreamEE, sep='|')
+    KO$event <- paste('RI', KO$chr, KO$strand, KO$riExonStart_0base, KO$riExonEnd, KO$upstreamES, KO$upstreamEE, KO$downstreamES, KO$downstreamEE, KO$geneSymbol, KO$GeneID, sep='|')
+    MT$event <- paste('RI', MT$chr, MT$strand, MT$riExonStart_0base, MT$riExonEnd, MT$upstreamES, MT$upstreamEE, MT$downstreamES, MT$downstreamEE, MT$geneSymbol, MT$GeneID, sep='|')
   }
   if (AS == 'MXE') {
-    KO$event <- paste(KO$chr, KO$strand, KO$X1stExonStart_0base, KO$X1stExonEnd, KO$X2ndExonStart_0base, KO$X2ndExonEnd, KO$upstreamES, KO$upstreamEE, KO$downstreamES, KO$downstreamEE, sep='|')
-    MT$event <- paste(MT$chr, MT$strand, MT$X1stExonStart_0base, MT$X1stExonEnd, MT$X2ndExonStart_0base, MT$X2ndExonEnd, MT$upstreamES, MT$upstreamEE, MT$downstreamES, MT$downstreamEE, sep='|')
+    KO$event <- paste('MXE', KO$chr, KO$strand, KO$X1stExonStart_0base, KO$X1stExonEnd, KO$X2ndExonStart_0base, KO$X2ndExonEnd, KO$upstreamES, KO$upstreamEE, KO$downstreamES, KO$downstreamEE, KO$geneSymbol, KO$GeneID, sep='|')
+    MT$event <- paste('MXE', MT$chr, MT$strand, MT$X1stExonStart_0base, MT$X1stExonEnd, MT$X2ndExonStart_0base, MT$X2ndExonEnd, MT$upstreamES, MT$upstreamEE, MT$downstreamES, MT$downstreamEE, MT$geneSymbol, MT$GeneID, sep='|')
   }
   if (AS == 'A3SS') {
-    KO$event <- paste(KO$chr, KO$strand, KO$longExonStart_0base, KO$longExonEnd, KO$shortES, KO$shortEE, KO$flankingES, KO$flankingEE, sep='|')
-    MT$event <- paste(MT$chr, MT$strand, MT$longExonStart_0base, MT$longExonEnd, MT$shortES, MT$shortEE, MT$flankingES, MT$flankingEE, sep='|')
+    KO$event <- paste('A3SS', KO$chr, KO$strand, KO$longExonStart_0base, KO$longExonEnd, KO$shortES, KO$shortEE, KO$flankingES, KO$flankingEE, KO$geneSymbol, KO$GeneID, sep='|')
+    MT$event <- paste('A3SS', MT$chr, MT$strand, MT$longExonStart_0base, MT$longExonEnd, MT$shortES, MT$shortEE, MT$flankingES, MT$flankingEE, MT$geneSymbol, MT$GeneID, sep='|')
   }
   if (AS == 'A5SS') {
-    KO$event <- paste(KO$chr, KO$strand, KO$longExonStart_0base, KO$longExonEnd, KO$shortES, KO$shortEE, KO$flankingES, KO$flankingEE, sep='|')
-    MT$event <- paste(MT$chr, MT$strand, MT$longExonStart_0base, MT$longExonEnd, MT$shortES, MT$shortEE, MT$flankingES, MT$flankingEE, sep='|')
+    KO$event <- paste('A5SS', KO$chr, KO$strand, KO$longExonStart_0base, KO$longExonEnd, KO$shortES, KO$shortEE, KO$flankingES, KO$flankingEE, KO$geneSymbol, KO$GeneID, sep='|')
+    MT$event <- paste('A5SS', MT$chr, MT$strand, MT$longExonStart_0base, MT$longExonEnd, MT$shortES, MT$shortEE, MT$flankingES, MT$flankingEE, MT$geneSymbol, MT$GeneID, sep='|')
   }
   
   KO_sig <- abs(KO$IncLevelDifference) > psi_cutoff & KO$Raw.p.values.Paired.test. < 0.05
@@ -61,8 +82,15 @@ pdf(paste0('figure/KO_T158M_AS_overlap_PSI', psi_cutoff, '.pdf'), wid=10)
 layout(matrix(c(1,2),nrow=1), wid=c(2,1))
 par(mar=c(2,2,2,2))
 par(xpd=T)
-venn(gene_lst)
+v <- venn(gene_lst)
 plot(0, type='n', xaxt='n', yaxt='n', bty='n')
 addtable2plot(0.45, 0, mat, display.rownames=F, display.colnames=T, bty='n')
 dev.off()
+
+intersections <- attr(v, 'intersections')
+olp_gene <- sub('\\..+', '', sub('.+\\|', '', intersections$`KO:Mutant`))
+go <- topGO(olp_gene, gomap='script/mouse_gene2GO.map', geneid='script/mouse_genewithGO')
+go$Term <- apply(go, 1, function(x){Term(GOTERM[[x[1]]])})
+write.table(go, paste0('table/KO_T158M_AS_overlap_PSI', psi_cutoff, '.GO.tsv'), sep = '\t', quote = F, row.names = F)
+write.table(intersections$`KO:Mutant`, paste0('table/KO_T158M_AS_overlap_PSI', psi_cutoff, '.events.tsv'), sep = '\t', quote = F, row.names = F, col.names = F)
 
--- script/combine_bg_events.R
+++ script/combine_bg_events.R
@@ -5,22 +5,24 @@ paths <- commandArgs(T)
 for (p in paths) {
   for (AS in c('SE', 'RI', 'MXE', 'A3SS', 'A5SS', 'novelEvents.SE', 'novelEvents.RI', 'novelEvents.MXE', 'novelEvents.A3SS', 'novelEvents.A5SS')) {
     KO <- read.table(paste0(p, '/fromGTF.', AS, '.txt'), header = T, sep = '\t')
-    if (AS == 'SE' || AS == 'novelEvents.SE') {
-      KO$event <- paste(KO$chr, KO$strand, KO$exonStart_0base, KO$exonEnd, KO$upstreamES, KO$upstreamEE, KO$downstreamES, KO$downstreamEE, sep='|')
+    if (nrow(KO) > 0) {
+      if (AS == 'SE' || AS == 'novelEvents.SE') {
+        KO$event <- paste('SE', KO$chr, KO$strand, KO$exonStart_0base, KO$exonEnd, KO$upstreamES, KO$upstreamEE, KO$downstreamES, KO$downstreamEE, KO$geneSymbol, KO$GeneID, sep='|')
+      }
+      if (AS == 'RI' || AS == 'novelEvents.RI') {
+        KO$event <- paste('RI', KO$chr, KO$strand, KO$riExonStart_0base, KO$riExonEnd, KO$upstreamES, KO$upstreamEE, KO$downstreamES, KO$downstreamEE, KO$geneSymbol, KO$GeneID, sep='|')
+      }
+      if (AS == 'MXE' || AS == 'novelEvents.MXE') {
+        KO$event <- paste('MXE', KO$chr, KO$strand, KO$X1stExonStart_0base, KO$X1stExonEnd, KO$X2ndExonStart_0base, KO$X2ndExonEnd, KO$upstreamES, KO$upstreamEE, KO$downstreamES, KO$downstreamEE, KO$geneSymbol, KO$GeneID, sep='|')
+      }
+      if (AS == 'A3SS' || AS == 'novelEvents.A3SS') {
+        KO$event <- paste('A3SS', KO$chr, KO$strand, KO$longExonStart_0base, KO$longExonEnd, KO$shortES, KO$shortEE, KO$flankingES, KO$flankingEE, KO$geneSymbol, KO$GeneID, sep='|')
+      }
+      if (AS == 'A5SS' || AS == 'novelEvents.A5SS') {
+        KO$event <- paste('A5SS', KO$chr, KO$strand, KO$longExonStart_0base, KO$longExonEnd, KO$shortES, KO$shortEE, KO$flankingES, KO$flankingEE, KO$geneSymbol, KO$GeneID, sep='|')
+      }
+      all_events <- c(all_events, KO$event)
     }
-    if (AS == 'RI' || AS == 'novelEvents.RI') {
-      KO$event <- paste(KO$chr, KO$strand, KO$riExonStart_0base, KO$riExonEnd, KO$upstreamES, KO$upstreamEE, KO$downstreamES, KO$downstreamEE, sep='|')
-    }
-    if (AS == 'MXE' || AS == 'novelEvents.MXE') {
-      KO$event <- paste(KO$chr, KO$strand, KO$X1stExonStart_0base, KO$X1stExonEnd, KO$X2ndExonStart_0base, KO$X2ndExonEnd, KO$upstreamES, KO$upstreamEE, KO$downstreamES, KO$downstreamEE, sep='|')
-    }
-    if (AS == 'A3SS' || AS == 'novelEvents.A3SS') {
-      KO$event <- paste(KO$chr, KO$strand, KO$longExonStart_0base, KO$longExonEnd, KO$shortES, KO$shortEE, KO$flankingES, KO$flankingEE, sep='|')
-    }
-    if (AS == 'A5SS' || AS == 'novelEvents.A5SS') {
-      KO$event <- paste(KO$chr, KO$strand, KO$longExonStart_0base, KO$longExonEnd, KO$shortES, KO$shortEE, KO$flankingES, KO$flankingEE, sep='|')
-    }
-    all_events <- c(all_events, KO$event)
   }
 }
 
