57ad72d34de9732f9feb65df3a48d25ca417bda8
Fu Xing <bio.xfu@gmail.com>
2019-03-14 13:42:56 +0800

    update

--- script/AS_gene_anno.R
+++ script/AS_gene_anno.R
@@ -5,7 +5,7 @@ AS_file <- argv[2]
 output <- paste0(AS_file, '_gene_anno.tsv')
 
 AS <- read.table(AS_file, header = T)
-anno <- read.table(anno_file, sep='\t', header = T, quote = '', row.names = 1)
+anno <- read.table(anno_file, sep='\t', header = T, quote = '', row.names = 1, comment.char = "")
 AS_table <- merge(AS, anno, by.x = 2, by.y = 0, all.x = T)
 
 write.table(AS_table, output, row.names=F, sep='\t', quote=F)
